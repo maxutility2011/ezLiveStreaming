5b574357c368738fee4ffaf1db56c00970a23893
maxutility2011 <maxutility2011@gmail.com>
2024-05-15 13:47:15 +0000

    bin to bins

--- api_server/start_server.sh
+++ api_server/start_server.sh
@@ -1,2 +1,2 @@
 sudo nginx
-/home/streamer/bin/api_server_main -config=/home/streamer/conf/apt_server_config.json
+/home/streamer/bins/api_server_main -config=/home/streamer/conf/apt_server_config.json
