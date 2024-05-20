3c76632fa8f36e3c0e66c76f7b813ae1e17c6947
maxutility2011 <maxutility2011@gmail.com>
2024-05-20 20:37:44 +0000

    added hls_fmp4_init_filename 2

--- job/command.go
+++ job/command.go
@@ -548,6 +548,10 @@ func JobSpecToEncoderArgs(j LiveJobSpec, media_output_path string) ([]string, []
 		ffmpegArgs = append(ffmpegArgs, "-hls_time")
 		ffmpegArgs = append(ffmpegArgs, strconv.Itoa(j.Output.Segment_duration))
 
+		ffmpegArgs = append(ffmpegArgs, "-hls_fmp4_init_filename")
+		hls_fmp4_init_filename_value := "stream_%v/init.mp4"
+		ffmpegArgs = append(ffmpegArgs, hls_fmp4_init_filename_value)
+		
 		ffmpegArgs = append(ffmpegArgs, "-hls_segment_filename")
 		hls_segment_filename_value := "stream_%v/seg_%02d"
 		file_extention := ""
@@ -559,10 +563,6 @@ func JobSpecToEncoderArgs(j LiveJobSpec, media_output_path string) ([]string, []
 
 		hls_segment_filename_value += file_extention
 		ffmpegArgs = append(ffmpegArgs, hls_segment_filename_value)
-
-		ffmpegArgs = append(ffmpegArgs, "-hls_fmp4_init_filename")
-		hls_fmp4_init_filename_value := "stream_%v/init.mp4"
-		ffmpegArgs = append(ffmpegArgs, hls_fmp4_init_filename_value)
 
 		ffmpegArgs = append(ffmpegArgs, "-master_pl_name")
 		ffmpegArgs = append(ffmpegArgs, HLS_MASTER_PLAYLIST_FILE_NAME)
