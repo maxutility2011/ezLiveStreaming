d9c5958d8a677a683674d04242bccc8c9b2dba66
maxutility2011 <maxutility2011@gmail.com>
2024-05-21 19:38:37 +0000

    Use init_segments_to_upload to temporarily keep pending init segments 8

--- job/command.go
+++ job/command.go
@@ -269,7 +269,8 @@ func JobSpecToShakaPackagerArgs(job_id string, j LiveJobSpec, media_output_path 
 		audio_output += ("," + media_segment_template_prefix + media_segment_template)
 
 		if j.Output.Stream_type == HLS {
-			playlist_name := "playlist_name=" + media_output_path + output_label + ".m3u8"
+			//playlist_name := "playlist_name=" + media_output_path + output_label + ".m3u8"
+			playlist_name := "playlist_name=" + media_output_path + output_label + "/playlist.m3u8"
 			audio_output += ("," + playlist_name)
 
 			hls_group_id := "hls_group_id=" + output_label
