06c6cca0ca2dda09326b1bcacb6f9b929ad77ed3
maxutility2011 <maxutility2011@gmail.com>
2024-06-03 19:08:44 +0000

    debug messages in scheduler

--- scheduler/scheduler.go
+++ scheduler/scheduler.go
@@ -892,6 +892,8 @@ func main_server_handler(w http.ResponseWriter, r *http.Request) {
 
 		// Handle job stats report
 		for _, j_stats := range report.JobStatsReport {
+			fmt.Printf("Stats of jod Id = %s:\n", j_stats.Id)
+			fmt.Printf("  - Ingress_bandwidth_kbps: %d, ingest_bandwidth_threshold_kbps: %d\n", j_stats.Ingress_bandwidth_kbps, ingest_bandwidth_threshold_kbps)
 			j, ok := getJobById(j_stats.Id)
 			if ok {
 				if j_stats.Ingress_bandwidth_kbps > ingest_bandwidth_threshold_kbps { // Job is actively ingesting
